0fec1be0b8a7f823627617ec233453827ebe8b02
Amer Ahmed <amer.ahmed@studerande.plushogskolan.se>
2022-09-16 12:44:31 +0200

    Added readme

--- main.py
+++ main.py
@@ -2,11 +2,9 @@
 This is the main point of TennisGame to run
 version v1.0.0
 """
-from game import TennisGame
 
 def main():
     """The main function to run TennisGame"""
-    tennis_game =  TennisGame()
 
 if __name__ == 'main':
     main()
